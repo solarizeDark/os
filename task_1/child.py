e68d0ded85d1f756aaf4a8ad179fe68020ab8432
egor <magister47Ludi@gmail.com>
2021-10-14 15:30:39 +0300

    child

--- task_1/child.py
+++ task_1/child.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 import os
+import time
 import sys
-import random
 
 # author: Fedusiv
 # 11-901
 
-for i in range(int(sys.argv[1])):
-        child = os.fork()
-        if child > 0:
-                res = os.wait()
-                print('PARENT PROCESS\tChild with PID: {} stopped. Status: {}'.format(res[0], res[1]))
-        else:
-                os.execl(sys.executable, '"{}"'.format(sys.executable), './child.py', str(random.randint(5, 10)))
+print('CHILD  PROCESS\tChild in process with PID: {} arg: {}'.format(os.getpid(), sys.argv[1]))
+time.sleep(int(sys.argv[1]))
+#print('CHILD  PROCESS\tProcess with PID: {} ended'.format(os.getpid()))
+
+status = 0 if int(sys.argv[1]) % 2 == 0 else 1
+os._exit(status)
